b94b1b356d9039062b5fb1b49863d7176eae1cca
minhnghia <BrozennVN@gmail.com>
2023-09-15 09:32:57 +0700

    Init project

---
+++ Controllers/JobListingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DNTU_JOBS.Controllers
+{
+    public class JobListingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
