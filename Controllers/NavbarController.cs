c69477bec9b8033cd634cc36708c0555744f05f4
minhnghia <BrozennVN@gmail.com>
2023-09-22 10:25:06 +0700

    Edit main page

---
+++ Controllers/NavbarController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DNTU_JOBS.Controllers
+{
+    public class NavbarController : Controller
+    {
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("FindJob")]
+        public IActionResult FindJob()
+        {
+            return View();
+        }
+        [Route("About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+        [Route("Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        [Route("/auth/register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [Route("/auth/login")]
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
